c72c4701bb92d8a8c4835d2cb34a2fd34786da0c
tawkanthony8 <anthony.tawk@greybell.com>
2020-10-29 00:45:13 +0200

    anthony_update_authspec

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -14,3 +14,24 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   });
 });
+
+// import { AuthService } from './auth.service';
+// import { HttpClient } from '@angular/common/http';
+// import { of } from 'rxjs';
+
+// describe('AuthService', () => {
+//     it('should perform a post to /auth with email and password', () => {
+//       const email = 'email';
+//       const password = 'password';
+//       const httpClientStub: jasmine.SpyObj<HttpClient> = jasmine.createSpyObj(
+//         'http',
+//         ['post']
+//       );
+//       const authService = new AuthService(httpClientStub);
+//       httpClientStub.post.and.returnValue(of());
+
+//       authService.login(email, password);
+
+//       expect(httpClientStub.post).toHaveBeenCalledWith('/auth', { email, password });
+//     });
+// });
