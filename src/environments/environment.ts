b633656f26f3af1ac97acad0c2cf82f8b1dbb53c
tawkanthony8 <anthony.tawk@greybell.com>
2020-10-28 23:35:51 +0200

    anthony-new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,17 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environment : {
   production: false,
-  firebaseConfig = {
-    apiKey: "AIzaSyCyagyxN30j6mLThhsFVbqh29zz26mjXI0",
-    authDomain: "angular-assignment-i--team-ii.firebaseapp.com",
-    databaseURL: "https://angular-assignment-i--team-ii.firebaseio.com",
-    projectId: "angular-assignment-i--team-ii",
-    storageBucket: "angular-assignment-i--team-ii.appspot.com",
-    messagingSenderId: "128412327573",
-    appId: "1:128412327573:web:d75a2bfe007a53695b459c",
-    measurementId: "G-0LC115BBQW"
+  firebaseConfig : {
+    apiKey: 'AIzaSyCyagyxN30j6mLThhsFVbqh29zz26mjXI0',
+    authDomain: 'angular-assignment-i--team-ii.firebaseapp.com',
+    databaseURL: 'https://angular-assignment-i--team-ii.firebaseio.com',
+    projectId: 'angular-assignment-i--team-ii',
+    storageBucket: 'angular-assignment-i--team-ii.appspot.com',
+    messagingSenderId: '128412327573',
+    appId: '1:128412327573:web:d75a2bfe007a53695b459c',
+    measurementId: 'G-0LC115BBQW'
   }
 };
 
