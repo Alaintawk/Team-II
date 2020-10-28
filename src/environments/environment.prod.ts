c0f4087113090d8a6728afb17a6c522839c0777a
tawkanthony8 <anthony.tawk@greybell.com>
2020-10-28 17:23:51 +0200

    anthony's work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig = {
+    apiKey: "AIzaSyCyagyxN30j6mLThhsFVbqh29zz26mjXI0",
+    authDomain: "angular-assignment-i--team-ii.firebaseapp.com",
+    databaseURL: "https://angular-assignment-i--team-ii.firebaseio.com",
+    projectId: "angular-assignment-i--team-ii",
+    storageBucket: "angular-assignment-i--team-ii.appspot.com",
+    messagingSenderId: "128412327573",
+    appId: "1:128412327573:web:d75a2bfe007a53695b459c",
+    measurementId: "G-0LC115BBQW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig = {
+    apiKey: "AIzaSyCyagyxN30j6mLThhsFVbqh29zz26mjXI0",
+    authDomain: "angular-assignment-i--team-ii.firebaseapp.com",
+    databaseURL: "https://angular-assignment-i--team-ii.firebaseio.com",
+    projectId: "angular-assignment-i--team-ii",
+    storageBucket: "angular-assignment-i--team-ii.appspot.com",
+    messagingSenderId: "128412327573",
+    appId: "1:128412327573:web:d75a2bfe007a53695b459c",
+    measurementId: "G-0LC115BBQW"
+  }
 };
 
 /*
